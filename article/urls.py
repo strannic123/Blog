54761e6ca3fdc4f9184ef1311c60cdec952e9c3d
str <mng223322@gmail.com>
2020-03-28 20:33:38 +0800

    2

--- article/urls.py
+++ article/urls.py
@@ -1,7 +1,12 @@
 
 from django.urls import path
-from .views import ArticleListView
+
+from . import views
+from .views import ArticleListView, ArticleDetailView, edit_page
 
 urlpatterns = [
-    path('', ArticleListView.as_view(), name='article_list' ),
+
+    path('', ArticleListView.as_view(), name='article_list'),
+    path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail_list'),
+    path('edit-page', views.edit_page, name='edit_page'),
 ]
--- article/views.py
+++ article/views.py
@@ -5,4 +5,18 @@ from .models import Article
 
 class ArticleListView(ListView):
     model = Article
-    template_name = 'article/index.html'
+    template_name = 'index.html'
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'detail.html'
+    context_object_name = 'detail_page'
+
+
+def edit_page(request):
+    template = 'edit_page.html'
+    context = {
+        'list_articles': Article.objects.all().order_by('-id')
+    }
+    return render(request, template, context)
