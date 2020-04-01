333b5244164ed3dc97ee9a17361b9fba53889353
str <mng223322@gmail.com>
2020-04-01 17:22:07 +0800

    before registration

--- article/urls.py
+++ article/urls.py
@@ -2,13 +2,13 @@
 from django.urls import path
 
 from . import views
-from .views import ArticleListView, ArticleDetailView, edit_page
+from .views import ArticleListView, ArticleDetailView, ArticleCreateView
 
 urlpatterns = [
 
     path('', ArticleListView.as_view(), name='article_list'),
     path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail_list'),
-    path('edit-page', views.edit_page, name='edit_page'),
-    path('update-page/<int:pk>', views.update_page, name='update_page'),
-    path('delet-page/<int:pk>', views.delete_page, name='delete_page'),
+    path('edit-page', views.ArticleCreateView.as_view(), name='edit_page'),
+    path('update-page/<int:pk>', views.ArticleUpdateView.as_view(), name='update_page'),
+    path('delete-page/<int:pk>', views.ArticleDeleteView.as_view(), name='delete_page'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,6 +1,7 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
-from django.urls import reverse
-from django.views.generic import ListView, DetailView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Article
 from .forms import ArticleForm
 
@@ -16,45 +17,93 @@ class ArticleDetailView(DetailView):
     context_object_name = 'detail_page'
 
 
-def edit_page(request):
-    success = False
-    if request.method == 'POST':
-        form = ArticleForm(request.POST)
-        if form.is_valid():
-            form.save()
-            success = True
+class CustomSuccessMessageMixin:
+    @property
+    def success_msg(self):
+        return False
 
-    template = 'edit_page.html'
-    context = {
-        'list_articles': Article.objects.all().order_by('-id'),
-        'form': ArticleForm(),
-        'success': success
-    }
-    return render(request, template, context)
+    def form_valid(self, form):
+        messages.success(self.request, self.success_msg)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return '%s?id=%s' % (self.success_url, self.object.id)
 
 
-def update_page(request, pk):
-    get_article = Article.objects.get(pk=pk)
-    success_update = False
+class ArticleCreateView(CustomSuccessMessageMixin, CreateView):
+    model = Article
+    template_name = 'edit_page.html'
+    form_class = ArticleForm
+    success_url = reverse_lazy('edit_page')
+    success_msg = 'Запись создана'
 
-    if request.method == 'POST':
-        form = ArticleForm(request.POST, instance=get_article)
-        if form.is_valid():
-            form.save()
-            success_update = True
+    def get_context_data(self, **kwargs):
+        kwargs['list_articles'] = Article.objects.all().order_by('-id')
+        return super().get_context_data(**kwargs)
 
 
-    template = 'edit_page.html'
-    context = {
-        'get_article': get_article,
-        'update': True,
-        'form': ArticleForm(instance=get_article),
-        'success_update': success_update
-    }
-    return render(request, template, context)
+class ArticleUpdateView(CustomSuccessMessageMixin, UpdateView):
+    model = Article
+    template_name = 'edit_page.html'
+    form_class = ArticleForm
+    success_url = reverse_lazy('edit_page')
+    success_msg = 'Запись обновлена'
 
-def delete_page(request, pk):
-    get_article = Article.objects.get(pk=pk)
-    get_article.delete()
-    return redirect(reverse('edit_page'))
 
+    def get_context_data(self, **kwargs):
+        kwargs['update'] = True
+        return super().get_context_data(**kwargs)
+
+
+class ArticleDeleteView(CustomSuccessMessageMixin, DeleteView):
+    model = Article
+    template_name = 'edit_page.html'
+    success_url = reverse_lazy('edit_page')
+    success_msg = 'Запись удалена'
+
+    def post(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_msg)
+        return super().post(request)
+
+
+# def edit_page(request):
+#     success = False
+#     if request.method == 'POST':
+#         form = ArticleForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             success = True
+#
+#     template = 'edit_page.html'
+#     context = {
+#         'list_articles': Article.objects.all().order_by('-id'),
+#         'form': ArticleForm(),
+#         'success': success
+#     }
+#     return render(request, template, context)
+
+#
+# def update_page(request, pk):
+#     get_article = Article.objects.get(pk=pk)
+#     success_update = False
+#
+#     if request.method == 'POST':
+#         form = ArticleForm(request.POST, instance=get_article)
+#         if form.is_valid():
+#             form.save()
+#             success_update = True
+#
+#     template = 'edit_page.html'
+#     context = {
+#         'get_article': get_article,
+#         'update': True,
+#         'form': ArticleForm(instance=get_article),
+#         'success_update': success_update
+#     }
+#     return render(request, template, context)
+
+#
+# def delete_page(request, pk):
+#     get_article = Article.objects.get(pk=pk)
+#     get_article.delete()
+#     return redirect(reverse('edit_page'))
